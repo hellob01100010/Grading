971d2feaea032a0ca8b4b476a593899de6020597
scriptpi01 <88918870+scriptpi01@users.noreply.github.com>
2022-03-08 02:17:39 +0700

    Add files via upload

---
+++ grade.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, z;
+    cin >> a >> b >> c;
+    z = a + b + c;
+    if (z >= 80) {
+        cout << "A" << endl;
+    }
+    else if (z >= 75) {
+        cout << "B+" << endl;
+    }
+    else if (z >= 70) {
+        cout << "B" << endl;
+    }
+    else if (z >= 65) {
+        cout << "C+" << endl;
+    }
+    else if (z >= 60) {
+        cout << "C" << endl;
+    }
+    else if (z >= 55) {
+        cout << "D+" << endl;
+    }
+    else if (z >= 50) {
+        cout << "D" << endl;
+    }
+    else if (z >= 0) {
+        cout << "F" << endl;
+    }
+    return 0;
+}
